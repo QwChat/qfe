dbce1c9880a708b286a7fc77ffbd3f30e6212dce
TW-DEV-Wesley.Chen <huchen@thoughtworks.com>
2018-09-21 17:53:36 +0800

    change yarn

--- src/entries/index.tsx
+++ src/entries/index.tsx
@@ -1,26 +1,56 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from "react"
+import * as ReactDOM from "react-dom"
 
 import { Register, Login } from "./../services/user"
 
 Register({
-  nickname: "chenhuan",
-  password: "123456"
+    nickname: "chenhuan",
+    password: "123456"
 })
 
 Login({
-  nickname: "chenhuan",
-  password: "123456"
+    nickname: "chenhuan",
+    password: "123456"
 })
 
 interface IndexProps {}
 
-interface IndexState {}
-
-class Index extends React.Component<IndexProps, IndexState> {
-  public render(): JSX.Element {
-    return <span>Index test</span>;
-  }
+interface IndexState {
+    count: number
 }
 
-ReactDOM.render(<Index />, document.getElementById("reactApp"));
+class Index extends React.Component<IndexProps, IndexState> {
+    private timer: number = 0
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0
+        }
+    }
+
+    public render(): JSX.Element {
+        return <span>Index test, count is: {this.state.count}</span>
+    }
+
+    beginTimer() {
+        this.timer = window.setTimeout(() => {
+          console.log(performance.now())
+            this.setState(
+                {
+                    count: this.state.count + 1
+                },
+                this.beginTimer
+            )
+        }, 1000)
+    }
+
+    componentDidMount() {
+        this.beginTimer()
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.timer)
+    }
+}
+
+ReactDOM.render(<Index />, document.getElementById("reactApp"))
